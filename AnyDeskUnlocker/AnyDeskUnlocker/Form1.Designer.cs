c8f97b92b4a111a7c46e7a85a04b69feb3d504a9
s490-debug <s49037977@gmail.com>
2025-08-20 00:38:18 +0300

    Add files via upload

---
+++ AnyDeskUnlocker/AnyDeskUnlocker/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace AnyDeskUnlocker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(285, 307);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(221, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Unlock AnyDesk";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(66, 407);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(659, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Note: This will also log out from your AnyDesk account. This is not our fault if " +
+    "you don\'t remember account info.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "AnyDeskUnlocker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AnyDeskUnlocker/AnyDeskUnlocker/Form1.cs
@@ -0,0 +1,249 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnyDeskUnlocker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static bool IsAdministrator()
+        {
+            var identity = System.Security.Principal.WindowsIdentity.GetCurrent();
+            var principal = new System.Security.Principal.WindowsPrincipal(identity);
+            return principal.IsInRole(System.Security.Principal.WindowsBuiltInRole.Administrator);
+        }
+
+        static bool RegistryKeyExists(string keyPath)
+        {
+            try
+            {
+                using (var key = Registry.Users.OpenSubKey(keyPath))
+                {
+                    return key != null;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        static void DeleteFileIfExists(string path)
+        {
+            try
+            {
+                if (File.Exists(path))
+                    File.Delete(path);
+            }
+            catch {}
+        }
+
+        static void MoveFile(string source, string destination)
+        {
+            try
+            {
+                if (File.Exists(source))
+                {
+                    File.Move(source, destination);
+                }
+                else if (File.Exists(destination))
+                {
+                    File.Delete(destination);
+                    File.Move(source, destination);
+                }
+                else
+                {
+                    File.Copy(source, destination, true);
+                }
+            }
+            catch {}
+        }
+
+        static void CopyFile(string source, string destination)
+        {
+            try
+            {
+                if (File.Exists(source))
+                    File.Copy(source, destination, true);
+            }
+            catch {}
+        }
+
+        static void DeleteDirectoryIfExists(string path)
+        {
+            try
+            {
+                if (Directory.Exists(path))
+                    Directory.Delete(path, true);
+            }
+            catch {}
+        }
+
+        static void CopyDirectory(string sourceDir, string targetDir)
+        {
+            try
+            {
+                if (!Directory.Exists(sourceDir))
+                    return;
+
+                Directory.CreateDirectory(targetDir);
+
+                foreach (var file in Directory.GetFiles(sourceDir))
+                {
+                    var destFileName = Path.Combine(targetDir, Path.GetFileName(file));
+                    File.Copy(file, destFileName, true);
+                }
+
+                foreach (var dir in Directory.GetDirectories(sourceDir))
+                {
+                    var destDir = Path.Combine(targetDir, Path.GetFileName(dir));
+                    CopyDirectory(dir, destDir);
+                }
+            }
+            catch {}
+        }
+
+        static void DeleteAllFilesInDirectory(string dir)
+        {
+            try
+            {
+                if (!Directory.Exists(dir))
+                    return;
+
+                foreach (var file in Directory.GetFiles(dir))
+                {
+                    File.Delete(file);
+                }
+
+                foreach (var subdir in Directory.GetDirectories(dir))
+                {
+                    Directory.Delete(subdir, true);
+                }
+            }
+            catch {}
+        }
+
+        static void StartAnyDeskService()
+        {
+            try
+            {
+                using (ServiceController sc = new ServiceController("AnyDesk"))
+                {
+                    sc.Start();
+                    sc.WaitForStatus(ServiceControllerStatus.Running, TimeSpan.FromSeconds(30));
+
+                    while (sc.Status != ServiceControllerStatus.Running)
+                    {
+                        System.Threading.Thread.Sleep(500);
+                        sc.Refresh();
+                    }
+                }
+            }
+            catch {}
+        }
+
+        static void StopAnyDeskService()
+        {
+            try
+            {
+                using (ServiceController sc = new ServiceController("AnyDesk"))
+                {
+                    if (sc.Status != ServiceControllerStatus.Stopped && sc.Status != ServiceControllerStatus.StopPending)
+                    {
+                        sc.Stop();
+                        sc.WaitForStatus(ServiceControllerStatus.Stopped, TimeSpan.FromSeconds(30));
+
+                        while (sc.Status != ServiceControllerStatus.Stopped)
+                        {
+                            System.Threading.Thread.Sleep(500);
+                            sc.Refresh();
+                        }
+                    }
+
+                    foreach (var process in Process.GetProcessesByName("AnyDesk"))
+                    {
+                        process.Kill();
+                        process.WaitForExit();
+                    }
+                }
+            }
+            catch {}
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StopAnyDeskService();
+
+            string allUsersProfile = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string tempPath = Path.GetTempPath();
+
+            string anyDeskProfilePath = Path.Combine(allUsersProfile, "AnyDesk");
+            string appDataAnyDeskPath = Path.Combine(appData, "AnyDesk");
+
+            DeleteFileIfExists(Path.Combine(anyDeskProfilePath, "service.conf"));
+            DeleteFileIfExists(Path.Combine(appDataAnyDeskPath, "service.conf"));
+
+            string userConfSource = Path.Combine(appDataAnyDeskPath, "user.conf");
+            string userConfTemp = Path.Combine(tempPath, "user.conf");
+            CopyFile(userConfSource, userConfTemp);
+
+            string tempThumbnailsDir = Path.Combine(tempPath, "thumbnails");
+            DeleteDirectoryIfExists(tempThumbnailsDir);
+
+            string sourceThumbnailsDir = Path.Combine(appDataAnyDeskPath, "thumbnails");
+            CopyDirectory(sourceThumbnailsDir, tempThumbnailsDir);
+
+            DeleteAllFilesInDirectory(anyDeskProfilePath);
+            DeleteAllFilesInDirectory(appDataAnyDeskPath);
+
+            StartAnyDeskService();
+
+            string systemConfPath = Path.Combine(anyDeskProfilePath, "system.conf");
+            if (File.Exists(systemConfPath))
+            {
+                bool hasIdLine = false;
+                using (var reader = new StreamReader(systemConfPath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (line.Contains("ad.anynet.id="))
+                        {
+                            hasIdLine = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (hasIdLine)
+                {
+                    StopAnyDeskService();
+                    MoveFile(userConfTemp, userConfSource);
+
+                    CopyDirectory(tempThumbnailsDir, sourceThumbnailsDir);
+                    DeleteDirectoryIfExists(tempThumbnailsDir);
+
+                    StartAnyDeskService();
+                }
+            }
+
+            MessageBox.Show("Succesfully unlocked AnyDesk!", "AnyDeskUnlocker", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
